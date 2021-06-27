892211765623a16ede6f1802e673cf7543afd10a
ElieMbayed <elie.mbayed@isae.edu.lb>
2021-06-27 23:18:11 +0300

    TP3 done

--- tp3/question1/IHMPile.java
+++ tp3/question1/IHMPile.java
@@ -36,17 +36,21 @@ public class IHMPile extends JFrame implements ActionListener{
 
     public void actionPerformed(ActionEvent ae){
         if(ae.getActionCommand().equals("empiler")){
-
-            // à compléter
-
-            // en cas d'exception
-            //contenu.setText( /* à compléter */"" + " estPleine !");
-
+            try {
+                p.empiler(donnee.getText()); 
+                contenu.setText(p.toString());
+            } catch (PilePleineException e) {
+                contenu.setText(p.toString() + " estPleine !");
+            } catch (NumberFormatException nfe) {
+                contenu.setText(p.toString() + " estVide !" + nfe.getMessage());
+            }
         }else{
-
-            // à compléter
-            // en cas d'exception
-            //contenu.setText( /* à compléter */"" + " estVide !");
+             try {
+                sommet.setText(p.depiler().toString());
+                contenu.setText(p.toString());
+            } catch (PileVideException e) {
+                contenu.setText(p.toString() + "");
+            }
         }
     }
 
